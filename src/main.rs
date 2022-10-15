1502c1ba744b874c16b512fd69201d4daa4be053
Staninna <26458805+Staninna@users.noreply.github.com>
2022-10-15 16:29:07 +0200

    Basic api and web server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use rocket::{fs::FileServer, get, launch, routes, Build, Rocket};
+use rocket::{
+    fs::{relative, FileServer},
+    get, launch, routes, Build, Rocket,
+};
 
-#[get("/world")]
-fn hello_world() -> &'static str {
-    "Hello, world!"
+#[get("/ping")]
+fn ping_pong() -> &'static str {
+    "pong"
 }
 
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
-        .mount("/", FileServer::from("ui/")) // <-- Hosts the frontend if I not gonna use any kind of frontend framework
-        .mount("/hello", routes![hello_world])
+        .mount("/", FileServer::from(relative!("ui/"))) // <-- Hosts the frontend if I not gonna use any kind of frontend framework
+        .mount("/", routes![ping_pong])
 }

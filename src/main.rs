e1bf7dcab55f9185b0933a73d76513c77d3410a6
Staninna <26458805+Staninna@users.noreply.github.com>
2022-10-15 16:03:32 +0200

    Started with the api (kinda)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{fs::FileServer, get, launch, routes, Build, Rocket};
+
+#[get("/world")]
+fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", FileServer::from("ui/")) // <-- Hosts the frontend if I not gonna use any kind of frontend framework
+        .mount("/hello", routes![hello_world])
 }

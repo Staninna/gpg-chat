557f98c1bb1c534009616e85e9ee1fca93bd69f3
Staninna <26458805+Staninna@users.noreply.github.com>
2022-10-16 23:55:12 +0200

    Changed to a match statement

--- src/v1/routes/auth.rs
+++ src/v1/routes/auth.rs
@@ -1,4 +1,5 @@
 // Imports
+use crate::appconfig::{MAX_LEN_USERNAME, MIN_LEN_USERNAME};
 use json::object;
 use rocket::{http::Status, post, response::content::RawJson};
 
@@ -6,43 +7,40 @@ use rocket::{http::Status, post, response::content::RawJson};
 pub fn login(username: &str) -> RawJson<String> {
     let bad = Status::BadRequest;
     let ok = Status::Ok;
+    let min_len_username = MIN_LEN_USERNAME - 1;
 
-    // username is empty
-    if username.is_empty() {
-        return RawJson(
+    match username.len() {
+        // username is empty
+        0 => RawJson(
             object! {
                 "code": bad.code,
-                "message": "Username is empty"
+                "message": "Username can not be empty"
             }
             .dump(),
-        );
-    }
-    // username is to short
-    else if username.len() < 5 {
-        return RawJson(
+        ),
+        // username is too short
+        0..=min_len_username => RawJson(
             object! {
-                "code": bad.code,
-                "message": "Username is to short"
+                "code": ok.code,
+                "message": format!("Username of {} is too short", username.len())
             }
             .dump(),
-        );
-    }
-    // username is to long
-    else if username.len() > 20 {
-        return RawJson(
+        ),
+        // username is too long
+        MAX_LEN_USERNAME.. => RawJson(
             object! {
-                "code": bad.code,
-                "message": "Username is too long"
+                "code": ok.code,
+                "message": format!("Username of {} is too long", username.len())
             }
             .dump(),
-        );
+        ),
+        // username is valid
+        _ => RawJson(
+            object! {
+                "code": ok.code,
+                "message": "Successfully logged in with username"
+            }
+            .dump(),
+        ),
     }
-
-    RawJson(
-        object! {
-            "code": ok.code,
-            "message": "Successfully logged in"
-        }
-        .dump(),
-    )
 }

b10f6b8e72f8098f2d9432818fb9a30cf5072d25
Staninna <26458805+Staninna@users.noreply.github.com>
2022-10-19 23:15:16 +0200

    Added things that are in the sqlite db

--- src/v1/structures/user.rs
+++ src/v1/structures/user.rs
@@ -1,15 +1,20 @@
 #[allow(dead_code)]
+#[derive(Debug)]
 pub struct User {
-    username: String,
-    public_key: String,
+    pub id: i32,
+    pub username: String,
+    pub public_key: String, // TODO: Change to public key type
+    pub created_at: String, // TODO: Change to DateTime type
 }
 
 #[allow(dead_code)]
 impl User {
-    pub fn new(username: String, public_key: String) -> User {
+    pub fn new(id: i32, username: &str, public_key: &str, created_at: &str) -> User {
         User {
-            username,
-            public_key,
+            id,
+            username: String::from(username),
+            public_key: String::from(public_key),
+            created_at: String::from(created_at),
         }
     }
 }

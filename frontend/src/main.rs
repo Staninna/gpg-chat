5d705b6e9f30bc109c95cd5bd50a5f56e7fbaee6
Staninna <26458805+Staninna@users.noreply.github.com>
2022-11-01 13:11:30 +0100

    Started playing around with yew frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,47 @@
+// Imports
+use yew::prelude::*;
+
+struct Model {
+    value: i64,
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let state = use_state(|| Model { value: 0 });
+
+    let on_click = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1,
+            })
+        })
+    };
+
+    let paragraphs = (0..state.value)
+        .map(|_| html! { <p class={ "inline" }>{ "" }</p> })
+        .collect::<Html>();
+
+    if state.value == 69 {
+        html! {
+            <div>
+                <p>{"Nice!"}</p>
+                <button onclick={on_click}>{"Click me!"}</button>
+                { paragraphs }
+            </div>
+        }
+    } else {
+        html! {
+        <div>
+            <button onclick={on_click}>{ "Click me!" }</button>
+                <p> { state.value } </p>
+                { paragraphs }
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
